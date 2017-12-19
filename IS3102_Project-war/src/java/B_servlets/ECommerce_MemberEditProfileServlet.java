3d4275b8f0959e44dea876365669ecaeec1f9ccb
dearvae <lovevae7@gmail.com>
2017-12-19 17:20:33 +0800

    Add files via upload

---
+++ IS3102_Project-war/src/java/B_servlets/ECommerce_MemberEditProfileServlet.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package B_servlets;
+
+import CommonInfrastructure.AccountManagement.AccountManagementBeanLocal;
+import CommonInfrastructure.SystemSecurity.SystemSecurityBeanLocal;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author BN
+ */
+public class ECommerce_MemberEditProfileServlet extends HttpServlet{
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
