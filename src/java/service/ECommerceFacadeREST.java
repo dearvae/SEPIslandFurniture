c29b94a3346737d6875eb666c1ea25d0f7150882
dearvae <lovevae7@gmail.com>
2018-02-04 00:28:13 +0800

    Merge origin/master
    Conflicts:
    	IS3102_Project-war/web/B/SG/shoppingCart.jsp

--- src/java/service/ECommerceFacadeREST.java
+++ src/java/service/ECommerceFacadeREST.java
@@ -1,5 +1,6 @@
 package service;
 
+import Entity.Countryentity;
 import Entity.Lineitementity;
 import Entity.Member;
 import java.net.URI;
@@ -8,6 +9,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.persistence.EntityManager;
 import javax.ws.rs.core.Context;
@@ -18,6 +21,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -28,7 +35,7 @@ public class ECommerceFacadeREST {
     private UriInfo context;
 
     public ECommerceFacadeREST() {
-   
+
     }
 
     @GET
@@ -37,64 +44,80 @@ public class ECommerceFacadeREST {
         //TODO return proper representation object
         throw new UnsupportedOperationException();
     }
-    
+
     @PUT
     @Path("createEcommerceTransactionRecord")
     @Produces({"application/json"})
-     public Response createEcommerceTransactionRecord(@QueryParam("memberID") Long memberID, @QueryParam("amountPaid") double amountPaid,
-             @QueryParam("countryID") Long countryID) {
-       try {
+    public Response createEcommerceTransactionRecord(@QueryParam("memberID") Long memberID, @QueryParam("amountPaid") double amountPaid,
+            @QueryParam("countryID") Long countryID) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        String currentDate = dateFormat.format(date); //2016/11/16 12:08:43
+        String currency = getCurrency(countryID);
+        try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/islandfurniture-it07?user=root&password=12345");
-        //    String stmt = "UPDATE salesrecordentity set memberID=?, amountPaid=?, countryID=? WHERE id=?";
-           String stmt = "";  
+            String stmt = "INSERT INTO salesrecordentity (AMOUNTDUE, AMOUNTPAID, AMOUNTPAIDUSINGPOINTS,CREATEDDATE,CURRENCY,LOYALTYPOINTSDEDUCTED,POSNAME,MEMBER_ID,STORE_ID)VALUES (?,?, 0,?,?,0,Counter 1,?,59);SELECT SCOPE_IDENTITY();";
             PreparedStatement ps = conn.prepareStatement(stmt);
-            ps.setLong(1, memberID);
+            ps.setDouble(1, amountPaid);
             ps.setDouble(2, amountPaid);
-            ps.setLong(3,countryID);
-            int result =ps.executeUpdate();
-            if(result>0){
-                     conn.close();
-                return Response.status(200).build();
-            }
+            ps.setString(3, currentDate);
+            ps.setString(4, currency);
+            ps.setLong(5, memberID);
+
+            int id = ps.executeUpdate();
+            conn.close();
+            return Response
+                    .status(201)
+                    .entity(id)
+                    .build();
         } catch (Exception ex) {
-            ex.printStackTrace();     
+            ex.printStackTrace();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
-     }
-     
-     
-     
-        @PUT
-        @Path("createEcommerceLineItemRecord")
-        @Produces({"application/json"})
-        public Response createEcommerceLineItemRecord(@QueryParam("salesRecordID") Long salesRecordID, @QueryParam("itemEntityID") Long itemEntityID,
-             @QueryParam("quantity") Integer quantity, @QueryParam("countryID") Long countryID) {
-                try {
+    }
+
+    public String getCurrency(Long countryID) {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client
+                .target("http://localhost:8080/SEPWebService-Student/webresources/entity.countryentity")
+                .path("get")
+                .queryParam("id", countryID);
+        Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.get();
+        Countryentity result = (Countryentity) response.readEntity(Countryentity.class);
+        return result.getCurrency();
+    }
+
+    @PUT
+    @Path("createEcommerceLineItemRecord")
+    @Produces({"application/json"})
+    public Response createEcommerceLineItemRecord(@QueryParam("salesRecordID") Long salesRecordID, @QueryParam("itemEntityID") Long itemEntityID,
+            @QueryParam("quantity") Integer quantity, @QueryParam("countryID") Long countryID) {
+        try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/islandfurniture-it07?user=root&password=12345");
-       //     String stmt = "UPDATE salesrecordentity_lineitementity set salesRecordID=?, itemEntityID=?, quantity=?, countryID=? WHERE id=?";
+            //     String stmt = "UPDATE salesrecordentity_lineitementity set salesRecordID=?, itemEntityID=?, quantity=?, countryID=? WHERE id=?";
             String stmt = "";
             PreparedStatement ps = conn.prepareStatement(stmt);
             ps.setLong(1, salesRecordID);
             ps.setLong(2, itemEntityID);
             ps.setInt(3, quantity);
-            ps.setLong(4,countryID);
-            int result =ps.executeUpdate();
-            if(result>0){
-                     conn.close();
+            ps.setLong(4, countryID);
+            int result = ps.executeUpdate();
+            if (result > 0) {
+                conn.close();
                 return Response.status(200).build();
             }
         } catch (Exception ex) {
-            ex.printStackTrace();     
+            ex.printStackTrace();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
-     }
+    }
 
 }
 
-    /**
-     * PUT method for updating or creating an instance of ECommerce
-     *
-     * @param content representation for the resource
-     * @return an HTTP response with content of the updated or created resource.
-     */
-
+/**
+ * PUT method for updating or creating an instance of ECommerce
+ *
+ * @param content representation for the resource
+ * @return an HTTP response with content of the updated or created resource.
+ */
